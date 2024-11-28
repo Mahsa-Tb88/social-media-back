ad6d011202f2cbd6c1dfab0edcbff00bdf285090
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2024-11-27 20:51:23 -0800

    module

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: true,
-    credential: true,
+    credentials: true,
   })
 );
 app.use(responseMiddleware);
--- middlewares/authMiddleWare.js
+++ middlewares/authMiddleWare.js
@@ -4,4 +4,5 @@ export async function checkToken(req, res, next) {
     try {
     } catch (error) {}
   }
+  next();
 }
